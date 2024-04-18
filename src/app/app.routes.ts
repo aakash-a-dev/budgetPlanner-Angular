254cc6cfc0eac120fb15c57d769dd7beca6dc720
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-04-19 02:15:08 +0530

    Done Routing and Creating

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './budget-planner/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'budget-planner',
+        loadChildren:() => import('./budget-planner/budget-planner-routing.module')
+            .then(m => m.BudgetPlannerRoutingModule)
+    }
+];
