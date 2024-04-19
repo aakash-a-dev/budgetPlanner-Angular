99a81d6518b4c2414fc2e2cfb6455746462f12f6
Aakash <126223325+aakash-a-dev@users.noreply.github.com>
2024-04-19 12:43:20 +0530

    Navbar done

--- src/app/budget-planner/budget-planner-routing.module.ts
+++ src/app/budget-planner/budget-planner-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    title: 'Login'
+  },
+  {
+    path: 'navbar',
+    component: NavbarComponent,
+
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    title: 'Dashboard'
   }
 ];
 
